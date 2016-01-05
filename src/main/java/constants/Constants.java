8410ba42732cb0ef6ffe019718ea8c1f059d5abc
ACM-Unit <viacheslav.koshchii@gmail.com>
2016-01-05 07:08:35 +0200

    New commit

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -2,7 +2,7 @@ package constants;
 
 public interface Constants {
     //	public static final String CONNECTING_URL = "jdbc:mysql://127.0.0.1/studentprogress?user=root&password=1515&characterEncoding=UTF-8";
-    String CONNECTING_URL = "jdbc:mysql://localhost:3306/students?user=root&password=root&characterEncoding=UTF-8";
+    String CONNECTING_URL = "jdbc:mysql://127.13.153.130:3306/jbossews?user=adminyGBDPY3&password=n5t4mmdF8VHU&characterEncoding=UTF-8";
     int CONNECTING_POOL_SIZE = 5;
 
     String CONTEXT = "CONTEXT";
